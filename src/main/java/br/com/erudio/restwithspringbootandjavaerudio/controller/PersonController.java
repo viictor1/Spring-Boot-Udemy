5486dd661e000f4d8e82b90794908ccb3654015d
Victor <victorgaspalmeida@hotmail.com>
2023-07-22 23:52:13 -0300

    content negotiation

--- src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controller/PersonController.java
@@ -2,10 +2,9 @@ package br.com.erudio.restwithspringbootandjavaerudio.controller;
 
 import br.com.erudio.restwithspringbootandjavaerudio.dto.PersonDto;
 import br.com.erudio.restwithspringbootandjavaerudio.dto.v2.dto.PersonDtoV2;
-import br.com.erudio.restwithspringbootandjavaerudio.mapper.custom.PersonMapper;
 import br.com.erudio.restwithspringbootandjavaerudio.service.PersonService;
+import br.com.erudio.restwithspringbootandjavaerudio.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,33 +18,33 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping(value = "/{id}",
-                produces = MediaType.APPLICATION_JSON_VALUE)
+                produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public PersonDto findById(@PathVariable(value = "id") Long id){
         return personService.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public List<PersonDto> findAll(){
         return personService.findAll();
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public PersonDto create(@RequestBody PersonDto person){
         return personService.create(person);
     }
 
     @PostMapping(value= "/v2",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public PersonDtoV2 createV2(@RequestBody PersonDtoV2 person){
         return personService.createV2(person);
     }
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
     public PersonDto update(@RequestBody PersonDto person){
         return personService.update(person);
     }
